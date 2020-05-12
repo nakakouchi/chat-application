f8b7c614de9fb653326ffbb6dec4b1026e242ed6
nakakouchi <61769004+nakakouchi@users.noreply.github.com>
2020-05-12 16:31:48 +0900

    tweet_index
    tweetのデータベースの作成
    データの表示

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -1,4 +1,5 @@
 class TopPageController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # get 'top_page/index'
-
   root "top_page#index"
+  resources :tweets, only: [:index]
 end
